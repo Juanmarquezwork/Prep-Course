160de9bf47878e38fc26a4e182a6154c8bb11bf2
Juanmarquezwork <juanmpcba@gmail.com>
2022-06-17 22:51:42 -0300

    homework 02 terminado 36/36

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -155,18 +155,18 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero<2){
+ if (numero === 0 || numero === 1 ){
+  return false;
+ }
+for (var p = 2; p < numero; p++){
+  if (numero%p === 0)
     return false;
-  }
-  if (numero===2){
-    return true
-  }
-  for (var p = 2; p < numero; p++) {
-    if (numero % p === 0) {
-      return false;
-    }
-  }
-  return true;
+  
+}
+return true;
+
+ 
+
 }
 
 function esVerdadero(valor){
@@ -184,7 +184,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  //CODIGO!!for (var S = 0; S <= 60; S++)
+  let tablaDelSeis = []
+  for (var S = 0; S <= 10; S++){
+     tablaDelSeis.push(6*S);
+  }
+  return tablaDelSeis;
 
   
 }
@@ -205,6 +209,8 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+return numero+40;
+
 }
 
 
